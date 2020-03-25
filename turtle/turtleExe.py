eeddc3db691b79a5c12c63fa274892b00606f43b
kian8502 <kian85@naver.com>
2020-03-25 13:52:36 +0900

    수동 자산 수동 리스크비율

--- turtle/turtleExe.py
+++ turtle/turtleExe.py
@@ -18,31 +18,33 @@ except OSError as e:
         print("Failed to create directory!!!!!")
         raise
 
-account=td.entireaccount()
+
 stockName=td.stockName()
-Limit=td.accountLimit(account,td.riskLimit)
+#Limit=td.accountLimit(account,td.riskLimit)
 
 
 i=1
 #반복횟수
 j=int(input("반복횟수: "))
 while i <= j:
+    account=td.entireaccount()
     print("%s회차"% i)
     enterP=td.firstPoint()
     N=float(input("N: "))
 
     with open(savePath+"%s.txt"% stockName, 'a') as f:
         now=td.date(input("오늘자 입력 1 or else :"))
+        riskPer=int(input("리스크 비율 : "))
         f.write("입력일 : "+str(now)+"\n")
         f.write("%s회차"% i+"\n")
         f.write("진입포인트 : "+str(enterP)+"\n")
         f.write("ATR : "+str(N)+"\n")
-        nextP=td.nextPoint(enterP,N)
-        f.write("정지포인트 : "+str(td.stopPoint(enterP,N))+"\n") #정지포인트
+        nextP=td.nextPoint(enterP,N,riskPer)
+        f.write("정지포인트 : "+str(td.stopPoint(enterP,N,riskPer))+"\n") #정지포인트
         f.write("다음포인트 : "+str(nextP)+"\n")
         Limit=td.accountLimit(account,td.riskLimit) #한도
         #f.write("거래한도 : "+str(Limit)+"\n") 
-        f.write("거래량 : "+str(td.contractAmount(Limit,N))+"\n") #거래량
+        f.write("거래량 : "+str(td.contractAmount(Limit,N,riskPer))+"\n") #거래량
         #f.write("계정금액 : "+str(td.changeAccount(account,N,i))+"\n") #계정금액
         f.write("\n")
     i+=1
