b8887b4b91ff216177dbdc938707639476a3d457
kian8502 <kian85@naver.com>
2020-04-05 10:32:18 +0900

    추가 수정
    Signed-off-by: kian8502 <kian85@naver.com>

--- turtle/turtleDef.py
+++ turtle/turtleDef.py
@@ -7,6 +7,7 @@ from datetime import datetime
 #종목명지정
 def stockName():
     stockName=input("종목명: ")
+    #name=stockName.encode('utf-8')
     return stockName
 #계정시작금액
 def entireaccount():
@@ -22,21 +23,14 @@ def accountLimit(account,riskPer):
 def firstPoint():
     enterPoint=int(input('첫진입포인트: '))
     return enterPoint
-#다음거래포인트
-def nextPoint(enterPoint,N,pramiding):
-    newEnterPoint=enterPoint+(N*pramiding)
-    print("다음거래포인트:",newEnterPoint)
-    return newEnterPoint 
+
 #거래량
 def contractAmount(Limit,N):
     contract=Limit//N
     print("거래량:",contract)
     return contract
-#거래정지포인트
-def stopPoint(enterPoint,N,pramiding):
-    stopPoint=enterPoint-(N*pramiding)
-    print("거래정지포인트:",stopPoint)
-    return stopPoint 
+
+
 #계정변동
 def changeAccount(account,N,i):
     account=account+(N*i)    
@@ -55,3 +49,19 @@ def price(enterPoint,amount):
     price=enterPoint*amount
     print("구매가격 : ",price)
     return price
+
+#거래정지포인트
+def stopPoint(enterPoint,N):
+    stopPoint=enterPoint-(N*2)
+    print("거래정지포인트:",stopPoint)
+    return stopPoint 
+#다음거래포인트
+def nextPoint(enterPoint,N,pramiding):
+    newEnterPoint=enterPoint+(N*pramiding)
+    print("다음거래포인트:",newEnterPoint)
+    return newEnterPoint
+#Trail 포인트
+def trailPoint(enterPoint,N):
+    trailP=enterPoint+(N*0.5)
+    print("Trail포인트:",trailP)
+    return trailP 
--- turtle/turtleExe.py
+++ turtle/turtleExe.py
@@ -20,44 +20,45 @@ except OSError as e:
         print("Failed to create directory!!!!!")
         raise
 
-
 stockName=td.stockName()
 #Limit=td.accountLimit(account,td.riskLimit)
+#account=td.entireaccount() #보유계정금
+account=1000000
+enterP=td.firstPoint() #첫진입 포인트
+N=float(input("N: "))
+riskPer=0.01
+now=td.date(input("오늘자 입력 1 or else :"))
+pramiding=float(input("피라미딩 포인트 0.5 or 1 : "))
+#기초정보
+def info(stockName):
+    with open(savePath+"%s.txt"% stockName, 'a', encoding="utf-8") as f:
+        f.write("입력일 : "+str(now)+"\n")
+        #f.write("%s회차"% i+"\n")
+        f.write("진입포인트 : "+str(enterP)+"\n")
+        f.write("ATR : "+str(N)+"\n")
+        f.write("\n")
+        Limit=td.accountLimit(account,riskPer) #한도
+        f.write("손실한도 : "+str(Limit)+"\n")
+        amount=(td.contractAmount(Limit,N)) #거래량
+        f.write("거래량 : "+str(amount)+"\n") 
+        f.write("구매금액 : "+str(td.price(enterP,amount))+"\n")
+        f.write("Pramiding포인트 : "+str(td.nextPoint(enterP,N,pramiding))+"\n") #추가포인트
+        f.write("\n")
+#포인트 추적
+class tradePoint:
+    with open(savePath+"%s.txt"% stockName, 'a', encoding="utf-8") as f:
+        f.write("Trail포인트 : "+str(td.trailPoint(enterP,N))+"\n") #트레일포인트
+        f.write("Drop포인트 : "+str(td.stopPoint(enterP,N))+"\n") #정지포인트
+        f.write("\n")
 
-def turtle(stockName):
-    i=1
-    #반복횟수
-    #j=int(input("반복횟수: "))
-    j=1
-    while i <= j:
-        account=td.entireaccount() #보유계정금
-        #print("%s회차"% i)
-        enterP=td.firstPoint() #첫진입 포인트
-        N=float(input("N: "))
-        riskPer=0.01
-        with open(savePath+"%s.txt"% stockName, 'a', encoding="utf-8") as f:
-            now=td.date(input("오늘자 입력 1 or else :"))
-            #riskPer=float(input("리스크 비율 : "))
-            pramiding=float(input("피라미딩 포인트 0.5 or 1 : "))
-            f.write("입력일 : "+str(now)+"\n")
-            #f.write("%s회차"% i+"\n")
-            f.write("진입포인트 : "+str(enterP)+"\n")
-            f.write("ATR : "+str(N)+"\n")
-            f.write("\n")
-            nextP=td.nextPoint(enterP,N,pramiding)
-            f.write("정지포인트 : "+str(td.stopPoint(enterP,N,pramiding))+"\n") #정지포인트
-            f.write("다음포인트 : "+str(nextP)+"\n")
-            f.write("\n")
-            Limit=td.accountLimit(account,riskPer) #한도
-            f.write("손실한도 : "+str(Limit)+"\n")
-            amount=(td.contractAmount(Limit,N)) #거래량
-            f.write("거래량 : "+str(amount)+"\n") 
-            #f.write("계정금액 : "+str(td.changeAccount(account,N,i))+"\n") #계정금액
-            f.write("구매금액 : "+str(td.price(enterP,amount))+"\n")
-            f.write("\n")
-        i+=1
+#turtle(stockName)
+call = input("1 = all, 2 = trail ")
+if call == "1":
+    info(stockName)
+    tp=tradePoint()
+if call == "2":
+    tp2=tradePoint()
 
-turtle(stockName)
 """ call = input("1 = turtle, 2 = record : ")
 
 if call == "1":
